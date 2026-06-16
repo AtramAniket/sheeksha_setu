6d01cf0cfe0c25d2620349345f4d958746991ab8
AtramAniket <aniket_atram@live.in>
2026-06-16 15:48:48 +0530

    added school model

--- app/models/school.py
+++ app/models/school.py
@@ -15,7 +15,7 @@ class School(Base):
 
 	name: Mapped[str] = mapped_column(String(150), nullable = False)
 
-	board: MappedOptional[st[r]] = mapped_column(String(100), nullable = True)
+	board: Mapped[Optional[str]] = mapped_column(String(100), nullable = True)
 
 	registration_number: MappedOptional[st[r]] = mapped_column(String(100), nullable = True)
 
