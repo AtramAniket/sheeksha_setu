baa14b3ee126ce1fe896dab3424d2d9aa62bcc96
AtramAniket <aniket_atram@live.in>
2026-06-16 15:10:45 +0530

    added logger middleware to main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
 from app.core.config import settings
+from app.core.logging import setup_logging
+from app.middleware import RequestLoggingMiddleware
+
+setup_logging()
 
 app = FastAPI(title = settings.APP_NAME)
 
+app.add_middleware(RequestLoggingMiddleware)
 
 @app.get("/")
 def root():
