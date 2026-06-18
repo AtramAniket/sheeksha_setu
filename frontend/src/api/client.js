4f4352936197d01499a6ed11abd0770bfcc8e9a7
AtramAniket <aniket_atram@live.in>
2026-06-18 15:29:50 +0530

    added basic config for api call

---
+++ frontend/src/api/client.js
@@ -0,0 +1,25 @@
+import axios from"axios";
+
+const apiClient = axios.create({
+	baseUrl: import.meta.env.VITE_API_BASE_URL,
+});
+
+apiClient.interceptors.request.use(
+	
+	(config) => {
+
+		const token = localStorage.getItem("access_token");
+
+		if(token){
+
+			config.headers.Authorization = `Bearer ${token}`
+
+			}
+
+		return config;
+
+		}
+	
+	)
+
+export default apiClient;
