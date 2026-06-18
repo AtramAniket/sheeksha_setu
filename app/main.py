e2650faf2966b9360c4c98d5aee98ff0335a634f
AtramAniket <aniket_atram@live.in>
2026-06-18 15:57:56 +0530

    added cors middleware and host for FastAPI

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from app.core.config import settings
 from app.api.v1.auth import auth_router
 from app.core.logging import setup_logging
+from fastapi.middleware.cors import CORSMiddleware
 from app.middleware import RequestLoggingMiddleware
 
 setup_logging()
@@ -10,6 +11,14 @@ setup_logging()
 app = FastAPI(title = settings.APP_NAME)
 
 app.add_middleware(RequestLoggingMiddleware)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 
